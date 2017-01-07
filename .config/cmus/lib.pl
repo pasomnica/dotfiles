8e512cafef9425526e401a4fb2a6ad6dc79b3eca
Dávid Kentoš <david.kentos@gmail.com>
2017-01-07 23:58:15 +0100

    Initial file commit

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,66 @@
+/home/kenty/Hudba/Advent.wav
+/home/kenty/Hudba/Creature Ate Mah Bass (Frequency Tester).wav
+/home/kenty/Hudba/deadmau5 - ebortS.wav
+/home/kenty/Hudba/Lost Empire.wav
+/home/kenty/Hudba/Nevermore.wav
+/home/kenty/Hudba/Redacted.wav
+/home/kenty/Hudba/Route 666.wav
+/home/kenty/Hudba/uKill.wav
+/home/kenty/Hudba/Unknown Origins - Grin VIP.wav
+/home/kenty/Hudba/Architecture In Tokyo & Macross 82 99 - City Nightlife (Original Mix) EXTRACT.mp3
+/home/kenty/Hudba/マクロスMACROSS 82-99 - Miss Macross.mp3
+/home/kenty/Hudba/04. Hood Pope.mp3
+/home/kenty/Hudba/06. Let It Bang (feat. ScHoolboy Q).mp3
+/home/kenty/Hudba/08. Yammy Gang (feat. A$AP Mob & Tatiana Paulino).mp3
+/home/kenty/Hudba/Bassnectar - Empathy [OFFICIAL].mp3
+/home/kenty/Hudba/06. Bonus.mp3
+/home/kenty/Hudba/Bones - DeadBoy/01 PaleEarnhardt.mp3
+/home/kenty/Hudba/Bones - DeadBoy/02 ArtesianWater.mp3
+/home/kenty/Hudba/Bones - DeadBoy/03 DeadBoy.mp3
+/home/kenty/Hudba/Bones - DeadBoy/04 TheGreatSnowfallOf82.mp3
+/home/kenty/Hudba/Bones - DeadBoy/05 Wi-Fi.mp3
+/home/kenty/Hudba/Bones - DeadBoy/06 1968.mp3
+/home/kenty/Hudba/Bones - DeadBoy/07 Lightning.mp3
+/home/kenty/Hudba/Bones - DeadBoy/08 Sixteen.mp3
+/home/kenty/Hudba/Bones - DeadBoy/09 WhiteLinenSheets.mp3
+/home/kenty/Hudba/Bones - DeadBoy/10 ConnectionLost.mp3
+/home/kenty/Hudba/Bones - DeadBoy/11 TimeshareCondominium.mp3
+/home/kenty/Hudba/Bones - DeadBoy/12 Calcium.mp3
+/home/kenty/Hudba/Bones - DeadBoy/13 Virus.mp3
+/home/kenty/Hudba/06. Okay, But This Is the Last Time.mp3
+/home/kenty/Hudba/12. Sixteen.mp3
+/home/kenty/Hudba/Bones - Useless/01 Useless (Prod. by Greaf).mp3
+/home/kenty/Hudba/Bones - Useless/02 RestInPeace (Prod. by The Virus And Antidote).mp3
+/home/kenty/Hudba/Bones - Useless/03 KeepTellingYourselfThat (Prod. by Vegard Veslelia).mp3
+/home/kenty/Hudba/Bones - Useless/04 GladWeHaveAnUnderstanding (Prod. by Dylan Brady).mp3
+/home/kenty/Hudba/Bones - Useless/05 Tempo (Prod. by Eric Dingus).mp3
+/home/kenty/Hudba/Bones - Useless/06 Soil (Prod. by Vegard Veslelia).mp3
+/home/kenty/Hudba/Bones - Useless/07 RightOnSchedule (Prod. by Stereoryze).mp3
+/home/kenty/Hudba/Bones - Useless/08 Translucent (Prod. by Curtis Heron).mp3
+/home/kenty/Hudba/Bones - Useless/09 (BONUS) Coinstar (Prod. by Fleece).mp3
+/home/kenty/Hudba/Daydreamer - Skyscrapers.mp3
+/home/kenty/Hudba/10. Raise Your Weapon (Original Mix).mp3
+/home/kenty/Hudba/10. Three Pound Chicken Wing.mp3
+/home/kenty/Hudba/13. Ain't Afraid.mp3
+/home/kenty/Hudba/ESPRIT 空想 - SUMMER NIGHT.mp3
+/home/kenty/Hudba/10. Low Life (feat. The Weeknd).mp3
+/home/kenty/Hudba/Goodie Mob - Cell Therapy.mp3
+/home/kenty/Hudba/15. II. zealots of stockholm [free information].mp3
+/home/kenty/Hudba/04. Gang Related.mp3
+/home/kenty/Hudba/Michael Jackson-Man in the mirror lyrics.mp3
+/home/kenty/Hudba/Michael Jackson Stranger in Moscow lyrics.mp3
+/home/kenty/Hudba/04. Sometimes Not Serious.mp3
+/home/kenty/Hudba/SchoolBoy Q - Collard Greens (Explicit) ft. Kendrick Lamar.mp3
+/home/kenty/Hudba/05. Scary Monsters And Nice Sprites (Phonat Remix).mp3
+/home/kenty/Hudba/The Strumbellas - Spirits.mp3
+/home/kenty/Hudba/twenty one pilots Heathens (from Suicide Squad The Album) [OFFICIAL VIDEO].mp3
+/home/kenty/Hudba/10. The Sacred.mp3
+/home/kenty/Hudba/29. Sleep Walk.mp3
+/home/kenty/Hudba/42. Lte.mp3
+/home/kenty/Hudba/43. Gloss of Blood.mp3
+/home/kenty/Hudba/45. In Order to Cast a Shadow You Must First Cast a Flame.mp3
+/home/kenty/Hudba/04. Private Caller.mp3
+/home/kenty/Hudba/04. Gatorade.mp3
+/home/kenty/Hudba/05. Hurt.mp3
+/home/kenty/Hudba/12. Solarflare.mp3
+/home/kenty/Hudba/Yung Lean - Volt.mp3
---
+++ .config/cmus/playlist.pl
@@ -0,0 +1,6 @@
+/home/kenty/Hudba/Bones - DeadBoy/12 Calcium.mp3
+/home/kenty/Hudba/Bones - DeadBoy/13 Virus.mp3
+/home/kenty/Hudba/Bones - DeadBoy/11 TimeshareCondominium.mp3
+/home/kenty/Hudba/Bones - DeadBoy/12 Calcium.mp3
+/home/kenty/Hudba/Bones - DeadBoy/11 TimeshareCondominium.mp3
+/home/kenty/Hudba/13. Ain't Afraid.mp3
